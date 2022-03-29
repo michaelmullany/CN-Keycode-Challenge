b39159ff5a160ba7cf55641e068876af1c891e00
Michael Mullany <mamullany@gmail.com>
2022-03-29 11:07:24 +0100

    Add show/hide functionality

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+const startText = document.getElementsByTagName("h2")[0];
 const codePara = document.getElementById("code");
 const keyPara = document.getElementById("key");
 const whichPara = document.getElementById("which");
 
-console.log(codePara);
+const infoBox = document.getElementById("key-info-container");
+console.log(infoBox);
+
 
 document.addEventListener("keypress", (event) => {
+    infoBox.style.display = "flex";
+    startText.display = "hidden";
+
+    startText.textContent = event.which;
     codePara.textContent = event.code;
     keyPara.textContent = event.key;
     whichPara.textContent = event.which;
